41e102cf8d6184ccb4b48a80237ddca743b32a3b
+ <+>
2024-01-15 15:41:00 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const matchingDrivers = [];
+
+    drivers.forEach(driver => {
+        if (driver.toLowerCase() === string.toLowerCase()){
+        matchingDrivers.push(driver);
+    }
+    });
+
+    return matchingDrivers;
+}
+
+
+
+function fuzzyMatch(drivers, string) {
+    const matchingDrivers = [];
+
+    drivers.forEach(driver => {
+        if (driver.toLowerCase().startsWith(string.toLowerCase())) {
+            matchingDrivers.push(driver);
+        }
+    })
+
+    return matchingDrivers;
+}
+
+function matchName(drivers, string) {
+    const matchingDrivers = [];
+    
+    drivers.forEach(driver => {
+        if (driver.name === string) {
+            matchingDrivers.push(driver);
+        }
+    });
+    
+    return matchingDrivers;
+}
